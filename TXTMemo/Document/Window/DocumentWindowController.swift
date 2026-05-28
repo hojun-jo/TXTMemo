77e7baa2464348f33bb58c971f8d3863d99988ed
Moon <1tjsen@naver.com>
2026-05-28 14:35:16 +0900

    feat: add save entrypoints

--- TXTMemo/Document/Window/DocumentWindowController.swift
+++ TXTMemo/Document/Window/DocumentWindowController.swift
@@ -4,23 +4,37 @@ import AppKit
 final class DocumentWindowController: NSWindowController, NSWindowDelegate, NSToolbarDelegate, NSTextFieldDelegate, NSMenuItemValidation {
     private enum Layout {
         static let controlHeight: CGFloat = 28
+        static let saveButtonWidth: CGFloat = 56
         static let fontSizeFieldWidth: CGFloat = 56
         static let toolbarButtonWidth: CGFloat = 32
         static let wrapButtonWidth: CGFloat = 72
     }
 
     private enum ToolbarItemIdentifier {
+        static let save = NSToolbarItem.Identifier("save")
         static let decreaseFontSize = NSToolbarItem.Identifier("decreaseFontSize")
         static let currentFontSize = NSToolbarItem.Identifier("currentFontSize")
         static let increaseFontSize = NSToolbarItem.Identifier("increaseFontSize")
         static let wrapToggle = NSToolbarItem.Identifier("wrapToggle")
     }
 
+    private static let defaultToolbarItemIdentifiers: [NSToolbarItem.Identifier] = [
+        ToolbarItemIdentifier.save,
+        ToolbarItemIdentifier.decreaseFontSize,
+        ToolbarItemIdentifier.currentFontSize,
+        ToolbarItemIdentifier.increaseFontSize,
+        ToolbarItemIdentifier.wrapToggle,
+        .flexibleSpace
+    ]
+
     private lazy var closeCoordinator = CloseWorkflowCoordinator(document: documentRef, windowController: self)
     private let documentRef: NotepadDocument
     private let sessionController = EditorSessionController()
     private weak var documentViewController: DocumentWindowViewController?
     private var bypassesCloseConfirmation = false
+    private let saveButton = NSButton(title: "Save", target: nil, action: nil)
+    private let decreaseFontSizeButton = NSButton(title: "A-", target: nil, action: nil)
+    private let increaseFontSizeButton = NSButton(title: "A+", target: nil, action: nil)
     private let fontSizeField = NSTextField(string: "")
     private let wrapToggleButton = NSButton(checkboxWithTitle: "Wrap", target: nil, action: nil)
     private var fontSizeObserverID: UUID?
@@ -61,6 +75,7 @@ final class DocumentWindowController: NSWindowController, NSWindowDelegate, NSTo
 
     override func synchronizeWindowTitleWithDocumentName() {
         super.synchronizeWindowTitleWithDocumentName()
+        refreshToolbarUI()
     }
 
     func windowShouldClose(_ sender: NSWindow) -> Bool {
@@ -91,6 +106,16 @@ final class DocumentWindowController: NSWindowController, NSWindowDelegate, NSTo
         documentViewController?.commitPendingEditorText()
     }
 
+    @objc func saveDocument(_ sender: Any?) {
+        prepareToSave()
+        documentRef.save(sender)
+    }
+
+    @objc func saveDocumentAs(_ sender: Any?) {
+        prepareToSave()
+        documentRef.saveAs(sender)
+    }
+
     @objc func increaseFontSize(_ sender: Any?) {
         documentViewController?.increaseFontSize()
     }
@@ -117,6 +142,10 @@ final class DocumentWindowController: NSWindowController, NSWindowDelegate, NSTo
 
     func validateMenuItem(_ menuItem: NSMenuItem) -> Bool {
         switch menuItem.action {
+        case #selector(saveDocument(_:)):
+            return documentRef.isDocumentEdited
+        case #selector(saveDocumentAs(_:)):
+            return true
         case #selector(increaseFontSize(_:)):
             return currentFontSize < FontSizePolicy.maximum
         case #selector(decreaseFontSize(_:)):
@@ -132,23 +161,11 @@ final class DocumentWindowController: NSWindowController, NSWindowDelegate, NSTo
     }
 
     func toolbarAllowedItemIdentifiers(_ toolbar: NSToolbar) -> [NSToolbarItem.Identifier] {
-        [
-            ToolbarItemIdentifier.decreaseFontSize,
-            ToolbarItemIdentifier.currentFontSize,
-            ToolbarItemIdentifier.increaseFontSize,
-            ToolbarItemIdentifier.wrapToggle,
-            .flexibleSpace
-        ]
+        Self.defaultToolbarItemIdentifiers
     }
 
     func toolbarDefaultItemIdentifiers(_ toolbar: NSToolbar) -> [NSToolbarItem.Identifier] {
-        [
-            .flexibleSpace,
-            ToolbarItemIdentifier.decreaseFontSize,
-            ToolbarItemIdentifier.currentFontSize,
-            ToolbarItemIdentifier.increaseFontSize
-            , ToolbarItemIdentifier.wrapToggle
-        ]
+        Self.defaultToolbarItemIdentifiers
     }
 
     func toolbar(
@@ -157,12 +174,15 @@ final class DocumentWindowController: NSWindowController, NSWindowDelegate, NSTo
         willBeInsertedIntoToolbar flag: Bool
     ) -> NSToolbarItem? {
         switch itemIdentifier {
+        case ToolbarItemIdentifier.save:
+            let item = NSToolbarItem(itemIdentifier: itemIdentifier)
+            item.view = saveButton
+            item.label = "Save"
+            item.paletteLabel = "Save"
+            return item
         case ToolbarItemIdentifier.decreaseFontSize:
             let item = NSToolbarItem(itemIdentifier: itemIdentifier)
-            let button = NSButton(title: "A-", target: self, action: #selector(decreaseFontSize(_:)))
-            button.bezelStyle = .texturedRounded
-            button.setFrameSize(NSSize(width: Layout.toolbarButtonWidth, height: Layout.controlHeight))
-            item.view = button
+            item.view = decreaseFontSizeButton
             item.label = "A-"
             item.paletteLabel = "Decrease Font Size"
             return item
@@ -173,10 +193,7 @@ final class DocumentWindowController: NSWindowController, NSWindowDelegate, NSTo
             return item
         case ToolbarItemIdentifier.increaseFontSize:
             let item = NSToolbarItem(itemIdentifier: itemIdentifier)
-            let button = NSButton(title: "A+", target: self, action: #selector(increaseFontSize(_:)))
-            button.bezelStyle = .texturedRounded
-            button.setFrameSize(NSSize(width: Layout.toolbarButtonWidth, height: Layout.controlHeight))
-            item.view = button
+            item.view = increaseFontSizeButton
             item.label = "A+"
             item.paletteLabel = "Increase Font Size"
             return item
@@ -201,11 +218,11 @@ final class DocumentWindowController: NSWindowController, NSWindowDelegate, NSTo
     private func bindSessionState() {
         fontSizeObserverID = sessionController.addFontSizeObserver { [weak self] fontSize in
             self?.fontSizeField.stringValue = String(fontSize)
-            self?.window?.toolbar?.validateVisibleItems()
+            self?.refreshToolbarUI()
         }
         wrapObserverID = sessionController.addWrapObserver { [weak self] wrapEnabled in
             self?.wrapToggleButton.state = wrapEnabled ? .on : .off
-            self?.window?.toolbar?.validateVisibleItems()
+            self?.refreshToolbarUI()
         }
     }
 
@@ -221,6 +238,10 @@ final class DocumentWindowController: NSWindowController, NSWindowDelegate, NSTo
 
     private func configureToolbarControlsIfNeeded() {
         guard !hasConfiguredToolbarControls else { return }
+
+        configureToolbarButton(saveButton, width: Layout.saveButtonWidth, action: #selector(saveDocument(_:)))
+        configureToolbarButton(decreaseFontSizeButton, width: Layout.toolbarButtonWidth, action: #selector(decreaseFontSize(_:)))
+        configureToolbarButton(increaseFontSizeButton, width: Layout.toolbarButtonWidth, action: #selector(increaseFontSize(_:)))
 
         fontSizeField.alignment = .right
         fontSizeField.controlSize = .regular
@@ -248,6 +269,34 @@ final class DocumentWindowController: NSWindowController, NSWindowDelegate, NSTo
         ])
 
         hasConfiguredToolbarControls = true
+        refreshToolbarControlStates()
+    }
+
+    private func configureToolbarButton(_ button: NSButton, width: CGFloat, action: Selector) {
+        button.target = self
+        button.action = action
+        button.bezelStyle = .texturedRounded
+        button.frame = NSRect(x: 0, y: 0, width: width, height: Layout.controlHeight)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.widthAnchor.constraint(equalToConstant: width),
+            button.heightAnchor.constraint(equalToConstant: Layout.controlHeight)
+        ])
+    }
+
+    private func prepareToSave() {
+        documentViewController?.commitPendingEditorText(preservingEditorFocus: true)
+    }
+
+    private func refreshToolbarUI() {
+        refreshToolbarControlStates()
+        window?.toolbar?.validateVisibleItems()
+    }
+
+    private func refreshToolbarControlStates() {
+        saveButton.isEnabled = documentRef.isDocumentEdited
+        decreaseFontSizeButton.isEnabled = currentFontSize > FontSizePolicy.minimum
+        increaseFontSizeButton.isEnabled = currentFontSize < FontSizePolicy.maximum
     }
 
     private func commitFontSizeField() {
