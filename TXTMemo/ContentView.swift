3bf13c97f8dc15fecd2cde2a768804787e08647c
Moon <1tjsen@naver.com>
2026-05-28 12:04:30 +0900

    Initial Commit

---
+++ TXTMemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TXTMemo
+//
+//  Created by 조호준 on 5/28/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TXTMemo/TXTMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TXTMemoApp.swift
+//  TXTMemo
+//
+//  Created by 조호준 on 5/28/26.
+//
+
+import SwiftUI
+
+@main
+struct TXTMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
